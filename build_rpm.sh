91fe8cf05cfeccaffcb45f4abbe8ccd2b0b313ca
Stefan Baranoff <stefan.baranoff@trinitycyber.com>
2022-01-10 19:45:48 +0000

    update build for cent8

--- build_rpm.sh
+++ build_rpm.sh
@@ -3,7 +3,7 @@
 GIT_VERSION=$(git describe --tags --long)
 VERSION=$(echo $GIT_VERSION | perl -p -e 's/v([0-9]+\.[0-9]+\.[0-9]+)-([0-9]+)-(g[0-9a-f]+)/\1/')
 RELEASE=$(echo $GIT_VERSION | perl -p -e 's/v([0-9]+\.[0-9]+\.[0-9]+)-([0-9]+)-(g[0-9a-f]+)/\2\.\3/')
-perl -p -e "s/__VERSION__/${VERSION}/; s/__RELEASE__/${RELEASE}/;" tc-portal-s3-push.spec.in > tc-portal-s3-push.spec
+perl -p -e "s/__VERSION__/${VERSION}/; s/__RELEASE__/${RELEASE}%{?dist}/;" tc-portal-s3-push.spec.in > tc-portal-s3-push.spec
 
 rpmbuild --define "_topdir ${PWD}" \
          --define "_builddir ${PWD}" \
